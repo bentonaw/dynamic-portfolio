23f5ba459dc35b0a69eb443de81619c9c282eb4e
Huan <98620169+bentonaw@users.noreply.github.com>
2024-03-24 23:51:05 +0100

    fix try

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import MainPage from "./pages/Main/MainPage";
 
 function App() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename="/dynamic-portfolio">
 			<Routes>
 				<Route path="/" element={<Layout/>}>
 
