b03f84bd6c947e1d3bae9296d73decc170fafea8
Huan <98620169+bentonaw@users.noreply.github.com>
2024-03-29 02:59:29 +0100

    color changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,23 @@
 import { HashLink } from "react-router-hash-link";
+import { useEffect, useState } from "react";
 import "../styles/header.css";
 
 export default function Header() {
+	const [color, setColor] = useState("white");
+
+	useEffect(() => {
+		const backgroundColor = getComputedStyle(document.body).backgroundColor;
+		const [r, g, b] = backgroundColor.match(/\d+/g).map(Number);
+
+		// Simple check for brightness
+		const isDarkBackground = r * 0.299 + g * 0.587 + b * 0.114 < 186;
+
+		setColor(isDarkBackground ? "white" : "black");
+	}, []);
+
 	return (
-		<header>
-			<HashLink to="/" id="logo">
+		<header style={{ color: color }}>
+			<HashLink to="/" id="logo" style={{ color: color }}>
 				Huan&apos;s page
 			</HashLink>
 			<nav className="menu-options">
@@ -14,22 +27,28 @@ export default function Header() {
 				<input type="checkbox" id="nav-toggle" />
 				<ul className="header-menu">
 					<li>
-						<HashLink smooth to="#main">
+						<HashLink smooth to="#main" style={{ color: color }}>
 							Main
 						</HashLink>
 					</li>
 					<li>
-						<HashLink smooth to="#about-me">About</HashLink>
+						<HashLink smooth to="#about-me" style={{ color: color }}>
+							About
+						</HashLink>
 					</li>
 					<li>
-						<HashLink smooth to="#portfolio">Portfolio</HashLink>
+						<HashLink smooth to="#portfolio" style={{ color: color }}>
+							Portfolio
+						</HashLink>
 					</li>
 					<li>
-						<HashLink smooth to="#contact">Contact</HashLink>
+						<HashLink smooth to="#contact" style={{ color: color }}>
+							Contact
+						</HashLink>
 					</li>
 				</ul>
 			</nav>
-			<div className="burger-menu">
+			<div className="burger-menu" style={{ color: color }}>
 				<label htmlFor="nav-toggle">
 					<i className="fa-solid fa-bars"></i>
 				</label>
