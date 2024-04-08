1526e09d294f2602047a331361ff7f94d163a88c
Huan <98620169+bentonaw@users.noreply.github.com>
2024-04-09 00:52:31 +0200

    added contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,28 @@
-
+import "../Contact/contact.css";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+	return (
+		<div>
+			<div className="contact">
+				<h2>Say Hi!</h2>
+			</div>
+			<div>
+				<section className="contact contactlinks">
+					<div>
+						<a href="mailto:huan-yang.ooi@chasacademy.se">
+							huan-yang.ooi@chasacademy.se
+						</a>
+					</div>
+					<div>
+						<a href="https://www.linkedin.com/in/huan-yang-ooi-74a824127/">
+							LinkedIn
+						</a>
+					</div>
+				</section>
+				<div className="contact"></div>
+			</div>
+		</div>
+	);
+};
 
-export default Contact
+export default Contact;
