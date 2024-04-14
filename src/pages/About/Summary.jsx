cb367b8be4c45c36e774451e0e53c5b1a44ebed2
Huan <98620169+bentonaw@users.noreply.github.com>
2024-04-14 22:41:59 +0200

    removed trial functions

--- src/pages/About/Summary.jsx
+++ src/pages/About/Summary.jsx
@@ -1,26 +1,7 @@
-import gsap from "gsap-trial";
-import { SplitText } from "gsap-trial/SplitText";
-import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
-
-gsap.registerPlugin(SplitText, ScrollTrigger);
-
-let mySplitText = new SplitText(".split", { type: "lines" });
-let lines = mySplitText.lines;
-gsap.from(lines, {
-	xPercent: -100,
-	stagger: 0.4,
-	ease: "back.out",
-	duration: 1.2,
-  scrollTrigger: {
-	trigger: ".splashText",
-	toggleActions: "restart pause resume pause",
-	start: "top 30%",
-	markers: true,
-  }
-
-})
 
 const Summary = () => {
+	
+
 	return (
 		<div className="summary">
 			<h2 className="split">About Me:</h2>
