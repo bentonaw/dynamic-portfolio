c3cbdda02fb39b62fa9bc039e6818bd77317cdf4
Huan <98620169+bentonaw@users.noreply.github.com>
2024-04-15 23:59:25 +0200

    scrolltriggers added to sections

--- src/pages/About/Education.jsx
+++ src/pages/About/Education.jsx
@@ -1,13 +1,56 @@
 import data from "../../assets/data.json";
 import "../../styles/card.css";
 import "./about.css";
+import gsap from "gsap";
+import { useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Education = () => {
-  return (
+	useEffect(() => {
+		gsap.fromTo(
+		".educationTitle",
+			{ x: "100%" }, // Start from outside the screen to the right
+			{
+				x: "63%", // Move to the original position
+				duration: 2,
+				ease: "back.out",
+				scrollTrigger: {
+					trigger: ".summarySpace",
+					start: "top center",
+					end: "bottom center",
+					markers: true,
+					toggleActions: "restart pause reverse pause",
+					scrub: 3,
+				},
+			});
+
+		gsap.fromTo(
+			".eduCard",
+			{ x: "0%" }, // Start from outside the screen to the right
+			{
+				x: "195%", // Move to the original position
+				duration: 2,
+				ease: "back.out",
+				stagger: 0.5, // Add a 0.5 second delay between each card's animation
+				scrollTrigger: {
+					trigger: ".summarySpace",
+					start: "top center",
+					end: "bottom center",
+					markers: true,
+					toggleActions: "restart pause reverse pause",
+					scrub: 3,
+				},
+			}
+		);
+	}, []);
+
+	return (
 		<div className="education">
-			<h2>Education:</h2>
+			<h2 className="educationTitle title">Education:</h2>
 			{data.education.map((education, index) => (
-				<div key={index} className="card">
+				<div key={index} className="card eduCard">
 					<h2>{education.school}</h2>
 					<p>{education.degree}</p>
 					<p>{education.graduated}</p>
@@ -15,6 +58,6 @@ const Education = () => {
 			))}
 		</div>
 	);
-}
+};
 
-export default Education
+export default Education;
--- src/pages/About/Summary.jsx
+++ src/pages/About/Summary.jsx
@@ -1,12 +1,101 @@
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".summaryTitle", {
+// 	xPercent: 100,
+// 	duration: 2,
+// 	ease: "back.out",
+// 	scrollTrigger: {
+// 		trigger: ".summary",
+// 		start: "top center",
+// 		endTrigger: ".summaryText",
+// 		end: "top center",
+// 		markers: true,
+// 		toggleActions: "restart pause reverse pause",
+// 		scrub: 3,
+// 	},
+// });
+
+// gsap.to(".summaryText", {
+// 	xPercent: 100,
+// 	duration: 2,
+// 	ease: "back.out",
+// 	scrollTrigger: {
+// 		trigger: ".summary",
+// 		start: "top center",
+// 		endTrigger: ".summaryText",
+// 		end: "top center",
+// 		markers: true,
+// 		toggleActions: "restart pause reverse pause",
+// 		scrub: 3,
+// 	},
+// });
+
+// const tl = gsap.timeline({
+// 	scrollTrigger: {
+// 		trigger: ".summary",
+// 		start: "top center",
+// 		endTrigger: ".summaryText",
+// 		end: "top center",
+// 		markers: true,
+// 		toggleActions: "restart pause reverse pause",
+// 		scrub: 3,
+// 	},
+// });
+
+// tl.fromTo(
+// 	".summaryTitle",
+// 	{ x: "-100%" }, // Start from outside the screen to the left
+// 	{
+// 		x: "0%", // Move to the original position
+// 		duration: 2,
+// 		ease: "back.out",
+// 	}
+// ).fromTo(
+// 	".summaryText",
+// 	{ x: "-100%" }, // Start from outside the screen to the left
+// 	{
+// 		x: "0%", // Move to the original position
+// 		duration: 2,
+// 		ease: "back.out",
+// 	},
+// 	"-=1.5" // This will start the second animation 1.5 seconds before the first one ends
+// );
 
 const Summary = () => {
-	
+	useEffect(() => {
+		const tlSum = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".scroll",
+				start: "top center",
+				endTrigger: ".summaryText",
+				end: "top 45%",
+				markers: true,
+				toggleActions: "restart pause reverse pause",
+				scrub: 3,
+			},
+		});
+
+		tlSum.to(".summaryTitle", {
+			xPercent: 100,
+			duration: 6,
+			ease: "back.out",
+		}).to(".summaryText", {
+			xPercent: 168,
+			duration: 4,
+			ease: "back.out",
+		});
+		("<"); // Start the .summaryText animation just after the .summaryTitle animation ends
+	}, []);
 
 	return (
 		<div className="summary">
-			<h2 className="split">About Me:</h2>
+			<h2 className="summaryTitle title">About Me:</h2>
 			<div>
-				<p className="split">
+				<p className="summaryText">
 					I am a web developer with a passion for creating beautiful and
 					functional websites. I have experience with HTML, CSS, JavaScript,
 					React, and Node.js. I am always looking to learn new technologies and
