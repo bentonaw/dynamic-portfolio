9e75bd7fc366d4eb0b375b2f4e60df68bfd2eff0
Huan <98620169+bentonaw@users.noreply.github.com>
2024-04-22 01:31:47 +0200

    skills revised

--- src/pages/About/Experience.jsx
+++ src/pages/About/Experience.jsx
@@ -7,23 +7,6 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-// const Experience = () => {
-//   return (
-// 		<div className="experience">
-// 			{data.experience.map((experience, index) => (
-// 				<div key={index} className="card">
-// 					<h2>{experience.company}</h2>
-// 					<p>{experience.title}</p>
-// 					<p>{`${experience.from} - ${experience.to}`}</p>
-// 					<p>{experience.location}</p>
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
-// }
-
-// export default Experience
-
 function Experience() {
 	useEffect(() => {
 		const tlExp = gsap.timeline({
--- src/pages/Skills/DevSkills.jsx
+++ src/pages/Skills/DevSkills.jsx
@@ -1,15 +1,54 @@
 import data from "../../assets/data.json";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-const DevSkills = () => {
-  return (
-	<div className="skills">
-			{data.developerSkills.map((skills, index) => (
-				<div key={index} className="card">
-					<h2>{skills.name}</h2>
-				</div>
-			))}
-		</div>
-  )
+gsap.registerPlugin(ScrollTrigger);
+
+// const DevSkills = () => {
+//   return (
+// 	<div className="skills">
+// 			{data.developerSkills.map((skills, index) => (
+// 				<div key={index} className="card">
+// 					<h2>{skills.name}</h2>
+// 				</div>
+// 			))}
+// 		</div>
+//   )
+// }
+
+function DevSkills() {
+	useEffect(() => {
+		gsap.fromTo(
+			".devSkillCard",
+			{ x: "2300%" }, // Start from outside the screen to the right
+			{
+				x: "0%", // Move to the original position
+				duration: 12,
+				ease: "none",
+				stagger: 3, // Add a 0.5 second delay between each card's animation
+				scrollTrigger: {
+					trigger: ".experienceSpace",
+					start: "top 55%",
+					end: "top 45%",
+					endTrigger: ".skillsMisc",
+					markers: true,
+					toggleActions: "restart pause reverse pause",
+					scrub: true,
+				},
+			}
+		);
+	}, []);
+
+return (
+    <div className="skills">
+      {data.developerSkills.map((skills, index) => (
+        <div key={index} className="card devSkillCard">
+          <h2>{skills.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default DevSkills
